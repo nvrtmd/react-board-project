b725e57af1455bf219f3c7c601b658199db3e2cb
YUZAMIN <untmd.crmn@gmail.com>
2022-09-02 00:56:25 +0900

    :lipstick: add global style

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  @font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff')
+      format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;  
+  }
+  
+  body {
+    font-family: 'GmarketSansMedium';
+    font-size: 1.4rem;
+    line-height: 1.43;
+  }
+  
+  a, a:hover, a:focus {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  input, button {
+    background-color: transparent;
+    outline: none;
+    cursor: pointer;
+  }
+
+  ol, ul, li {
+    list-style: none;
+  }
+
+  img {
+    width: 100%;
+  }
+`;
+
+export default GlobalStyle;
