2f23c272247fe4e29be9820971a47f2b72650bdd
YUZAMIN <untmd.crmn@gmail.com>
2022-09-02 02:35:03 +0900

    :construction: add profile page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Board from "./components/pages/board/Board";
 import Signup from "./components/pages/user/Signup";
 import Signin from "./components/pages/user/Signin";
+import Profile from "./components/pages/user/Profile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Board />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/pages/user/Profile.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components/macro";
+import axios from "axios";
+import Layout from "../../../layout/layout";
+import { theme } from "../../../styles/theme";
+
+export default function Profile() {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    userId: "",
+    userPassword: "",
+    userName: "",
+    userNickname: "",
+    userPhone: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get(`/user/profile`, { withCredentials: true })
+      .then((res) => setUserData(res.data.data))
+      .catch((err) => {});
+  }, []);
+
+  console.log(userData);
+
+  return <Layout></Layout>;
+}
