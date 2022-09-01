7485674d9425a5ef79c80faf3c78f50aab98b01a
YUZAMIN <untmd.crmn@gmail.com>
2022-09-02 01:01:31 +0900

    :lipstick: create theme file

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+const color = {
+  primary: "#28277c",
+  secondary: "#4c4eb7",
+  lightPurple: "#8587f3",
+  white: "#fff",
+  black: "#0b0b0b",
+  grey: "#C6C6C6",
+  deepGrey: "#414040",
+};
+
+export const theme = {
+  color,
+};
