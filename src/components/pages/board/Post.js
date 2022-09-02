a5aa4ff56400fb16e743f8806d8da17adf557c32
YUZAMIN <untmd.crmn@gmail.com>
2022-09-02 21:06:19 +0900

    :sparkles: create post deletion feature

--- src/components/pages/board/Post.js
+++ src/components/pages/board/Post.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import styled from "styled-components/macro";
+import { theme } from "../../../styles/theme";
 import Layout from "../../../layout/layout";
 
 export default function Post() {
@@ -32,12 +34,49 @@ export default function Post() {
     getPostData();
   }, [params.postId]);
 
+  const handleDeleteButtonClick = async () => {
+    try {
+      await axios.delete(`/board/delete/${postData.postId}`, {
+        withCredentials: true,
+      });
+      navigate("/board/list");
+    } catch {
+      alert("삭제 권한이 없습니다.");
+    }
+  };
+
   return (
     <Layout>
       <div>{postData.postTitle}</div>
       <div>{postData.postContents}</div>
       <div>{postData.postViews}</div>
       <div>{postData.postDisplay}</div>
+      <ButtonWrapper>
+        <Button>수정</Button>
+        <Button onClick={handleDeleteButtonClick}>삭제</Button>
+        <Button onClick={() => navigate("/board/list")}>목록</Button>
+      </ButtonWrapper>
     </Layout>
   );
 }
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Button = styled.div`
+  background: ${theme.color.lightPurple};
+  width: 20%;
+  padding: 1rem 0px;
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  transition: all 0.3s ease;
+  cursor: pointer;
+  &:hover {
+    background: ${theme.color.primary};
+  }
+`;
