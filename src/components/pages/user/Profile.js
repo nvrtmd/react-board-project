fad17e40d410a03d4ca362fa99b84ad15742b4d3
YUZAMIN <untmd.crmn@gmail.com>
2022-09-02 15:45:19 +0900

    :bug: reproduce error situation to determine the cause

--- src/components/pages/user/Profile.js
+++ src/components/pages/user/Profile.js
@@ -4,34 +4,18 @@ import styled from "styled-components/macro";
 import axios from "axios";
 import Layout from "../../../layout/layout";
 import { theme } from "../../../styles/theme";
-import app from "../../../utils/axiosConfig";
 
 export default function Profile() {
   const navigate = useNavigate();
-  const [userData, setUserData] = useState({
-    userId: "",
-    userPassword: "",
-    userName: "",
-    userNickname: "",
-    userPhone: "",
-  });
-
-  // const getUserData = async () => {
-  //   return await fetch(`${process.env.REACT_APP_API_URL}/user/profile`, {
-  //     method: "GET",
-  //     withCredentials: true,
-  //     credentials: "include",
-  //     mode: "cors",
-  //   });
-  // };
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
-    // const data = getUserData();
-    // console.log(data);
     axios
       .get(`/user/profile`, { withCredentials: true })
       .then((res) => setUserData(res.data.data))
-      .catch((err) => {});
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   console.log(userData);
