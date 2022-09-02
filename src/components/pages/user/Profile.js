da602298d5576e066ae8211cb5ba38a09b24e732
YUZAMIN <untmd.crmn@gmail.com>
2022-09-02 17:40:00 +0900

    :sparkles: make temporary profile page

--- src/components/pages/user/Profile.js
+++ src/components/pages/user/Profile.js
@@ -1,24 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components/macro";
 import axios from "axios";
 import Layout from "../../../layout/layout";
-import { theme } from "../../../styles/theme";
 
 export default function Profile() {
-  const navigate = useNavigate();
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState({
+    userId: "",
+    userName: "",
+    userNickname: "",
+    userPhone: "",
+  });
+
+  const getUserData = async () => {
+    const fetchedUserData = await (
+      await axios.get(`/user/profile`, { withCredentials: true })
+    ).data.data;
+
+    setUserData({
+      userId: fetchedUserData.user_id,
+      userName: fetchedUserData.user_name,
+      userNickname: fetchedUserData.user_nickname,
+      userPhone: fetchedUserData.user_phone,
+    });
+  };
 
   useEffect(() => {
-    axios
-      .get(`/user/profile`, { withCredentials: true })
-      .then((res) => setUserData(res.data.data))
-      .catch((err) => {
-        console.log(err);
-      });
+    getUserData();
   }, []);
 
-  console.log(userData);
-
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <div>아이디: {userData.userId}</div>
+      <div>이름: {userData.userName}</div>
+      <div>닉네임: {userData.userNickname}</div>
+      <div>전화번호: {userData.userPhone}</div>
+    </Layout>
+  );
 }
