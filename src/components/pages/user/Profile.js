0df486d0864b23145229d3c813c11b4821552b9a
YUZAMIN <untmd.crmn@gmail.com>
2022-09-02 14:42:11 +0900

    :bug: change axios to fetch

--- src/components/pages/user/Profile.js
+++ src/components/pages/user/Profile.js
@@ -16,11 +16,21 @@ export default function Profile() {
     userPhone: "",
   });
 
+  const getUserData = async () => {
+    return await fetch(`${process.env.REACT_APP_API_URL}/user/profile`, {
+      method: "GET",
+      withCredentials: true,
+      credentials: "include",
+    });
+  };
+
   useEffect(() => {
-    axios
-      .get(`/user/profile`, { withCredentials: true })
-      .then((res) => setUserData(res.data.data))
-      .catch((err) => {});
+    const data = getUserData();
+    console.log(data);
+    // axios
+    //   .get(`/user/profile`, { withCredentials: true })
+    //   .then((res) => setUserData(res.data.data))
+    //   .catch((err) => {});
   }, []);
 
   console.log(userData);
